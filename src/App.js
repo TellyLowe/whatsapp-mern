c5605c6969417b2eec28ceb60a3e0408d71106a8
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-09-17 18:34:57 -0400

    Sidebar & Chat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a MERN Whatsapp clone</h1>
+      <Sidebar/>
+      <Chat />
     </div>
   );
 }
