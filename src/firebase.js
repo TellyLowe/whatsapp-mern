f56e3496cb88533bc7b05aa30f3f0d5d4050bbef
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-09-17 17:08:32 -0400

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDabs5OkTm5ZuD_B8nYn1bbQ_W1iyvwiyQ",
+    authDomain: "whatsapp-mern-b4b31.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-b4b31.firebaseio.com",
+    projectId: "whatsapp-mern-b4b31",
+    storageBucket: "whatsapp-mern-b4b31.appspot.com",
+    messagingSenderId: "440946842319",
+    appId: "1:440946842319:web:751706c09059bb40b975ba",
+    measurementId: "G-PEXNY28K0J"
+};
