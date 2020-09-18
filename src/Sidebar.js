cbe898d36b974030da704b700573218fc2e11d3c
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-09-17 21:25:05 -0400

    SideBar icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h1>I am the sidebar</h1>
+            <div className="sidebar__header">
+            <Avatar src="https://pkimgcdn.peekyou.com/3bd322d7c6ab72239c8816e4e58d0b6d.jpeg" />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
